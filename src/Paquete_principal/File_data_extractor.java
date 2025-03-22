de87493f068740e13bbfbc4cb7b545578feeb83e
Michi-soft <michi.soft.21@gmail.com>
2025-03-22 19:22:50 -0300

    Implementados algunos metodos de Data extractor.

--- src/Paquete_principal/File_data_extractor.java
+++ src/Paquete_principal/File_data_extractor.java
@@ -1,29 +1,57 @@
 package Paquete_principal;
 
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class File_data_extractor {
 	private String path_pelicula;
+	File pelicula;
 	
 	public File_data_extractor(String path_pelicula) {
 		this.path_pelicula = path_pelicula;
+		this.pelicula = new File(this.path_pelicula);
 	}
 	
 	public String extractNombrePelicula() {
-		return "";
+		String nombre = pelicula.getName();
+		int primera_aparicion_parentesis = nombre.indexOf("("); 
+		nombre = nombre.substring(0, primera_aparicion_parentesis-1); 
+		return nombre;
 	}
 	
 	public double extractTamanioCarpeta () {
-		return 0;
+		long tamanio = 0;
+		File[] archivos = pelicula.listFiles();
+		for (int i=0; i<archivos.length; i++) {
+			if (archivos[i].isFile())
+				tamanio =+ archivos[i].length();
+		}
+		return Math.floor(((double)tamanio/(1024*1024))*100)/100;
 	}
 	
 	public String extractAnioPelicula() {
-		return "";
+		String nombre = pelicula.getName();
+		String anio = "NOT FOUND";
+		Pattern pattern = Pattern.compile("[(][0-9]{4}[)]");
+		Matcher matcher = pattern.matcher(nombre);
+		if (matcher.find()) {
+			anio = nombre.substring(matcher.start()+1, matcher.end()-1);
+		}
+		return anio;
 	}
 	
 	public String extractResolucionPelicula() {
-		return "";
+		String nombre = pelicula.getName();
+		String resolucion = "NOT FOUND";
+		Pattern pattern = Pattern.compile("[0-9]{4}[p]");
+		Matcher matcher = pattern.matcher(nombre);
+		if (matcher.find()) {
+			resolucion = nombre.substring(matcher.start(), matcher.end()-1);
+		}
+		return resolucion;
 	}
-	
-	
+		
 	public boolean extractTieneSubtitulo () {
 		return false;
 	}
--- src/Paquete_principal/Pelicula.java
+++ src/Paquete_principal/Pelicula.java
@@ -11,8 +11,44 @@ public class Pelicula {
 	
 	public Pelicula (String path_pelicula) {
 		this.ruta_pelicula = path_pelicula;
-		
+		File_data_extractor fde = new File_data_extractor(this.ruta_pelicula);
+		this.nombre_pelicula = fde.extractNombrePelicula();
+		this.anio_pelicula = fde.extractAnioPelicula();
+		this.tamanio_pelicula = fde.extractTamanioCarpeta();
+		this.resolucion_pelicula = fde.extractResolucionPelicula();
+		this.tiene_subtitulo = fde.extractTieneSubtitulo();
+		this.tiene_poster = fde.extractTienePoster();
 	}
+
+	public String getRuta_pelicula() {
+		return ruta_pelicula;
+	}
+
+	public String getNombre_pelicula() {
+		return nombre_pelicula;
+	}
+
+	public String getAnio_pelicula() {
+		return anio_pelicula;
+	}
+
+	public double getTamanio_pelicula() {
+		return tamanio_pelicula;
+	}
+
+	public String getResolucion_pelicula() {
+		return resolucion_pelicula;
+	}
+
+	public boolean getTiene_subtitulo() {
+		return tiene_subtitulo;
+	}
+
+	public boolean getTiene_poster() {
+		return tiene_poster;
+	}
+	
+	
 	
 	
 	
