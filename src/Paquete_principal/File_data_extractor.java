095773eec5146e64a815d94e4c9d3fce2d501659
Michi-soft <michi.soft.21@gmail.com>
2025-03-22 19:03:59 -0300

    Creada la clase Data extractor y sus metodos.

---
+++ src/Paquete_principal/File_data_extractor.java
@@ -0,0 +1,35 @@
+package Paquete_principal;
+
+public class File_data_extractor {
+	private String path_pelicula;
+	
+	public File_data_extractor(String path_pelicula) {
+		this.path_pelicula = path_pelicula;
+	}
+	
+	public String extractNombrePelicula() {
+		return "";
+	}
+	
+	public double extractTamanioCarpeta () {
+		return 0;
+	}
+	
+	public String extractAnioPelicula() {
+		return "";
+	}
+	
+	public String extractResolucionPelicula() {
+		return "";
+	}
+	
+	
+	public boolean extractTieneSubtitulo () {
+		return false;
+	}
+	
+	public boolean extractTienePoster() {
+		return false;
+	}
+	
+}
--- src/Paquete_principal/Pelicula.java
+++ src/Paquete_principal/Pelicula.java
@@ -1,5 +1,19 @@
 package Paquete_principal;
 
 public class Pelicula {
-
+	private String ruta_pelicula;
+	private String nombre_pelicula;
+	private String anio_pelicula;
+	private double tamanio_pelicula;
+	private String resolucion_pelicula;
+	private boolean tiene_subtitulo;
+	private boolean tiene_poster;
+	
+	public Pelicula (String path_pelicula) {
+		this.ruta_pelicula = path_pelicula;
+		
+	}
+	
+	
+	
 }
